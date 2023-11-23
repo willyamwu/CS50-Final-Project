17e8e793272bfdf168481f38ebbdd3860dc5fa2f
AntongH <144275521+AntongH@users.noreply.github.com>
2023-11-22 20:10:09 -0500

    Test
    Test

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.jinja_env.filters["usd"] = usd
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
+#ANTONG BRANCHUSSY
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
 
